d11b86fe5c7dc2a0a6d16df27666c6a566816ef6
Astarag Mohapatra <40949756+Athe-kunal@users.noreply.github.com>
2023-03-18 16:19:02 -0400

    Update mlp.py

--- mlp.py
+++ mlp.py
@@ -18,7 +18,7 @@ import datetime
 from finrl import config
 from finrl.meta.preprocessor.yahoodownloader import YahooDownloader
 # from FinRL.finrl.meta.preprocessor.preprocessors import FeatureEngineer, data_split
-from finrl.meta.env_stock_trading.env_stocktrading_np import StockTradingEnv as StockTradingEnv_numpy 
+from env_stocktrading_np import StockTradingEnv as StockTradingEnv_numpy 
 from finrl.meta.data_processor import DataProcessor
 from finrl.plot import backtest_stats, backtest_plot, get_daily_return, get_baseline
 # from env import StockTradingEnv as StockTradingEnv_numpy
